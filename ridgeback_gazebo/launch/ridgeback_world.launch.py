57303d4c768909da7b9cfaff0f0a0c5988f42cf4
LCBW <lcbw99@gmail.com>
2023-05-18 18:40:17 -0500

    loads in gazebo, starting plugin development soon

---
+++ ridgeback_gazebo/launch/ridgeback_world.launch.py
@@ -0,0 +1,101 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration, PythonExpression
+
+
+def generate_launch_description():
+    # Get the launch directory
+    this_directory = get_package_share_directory('ridgeback_gazebo')
+
+    # Launch configuration variables specific to simulation
+    headless = LaunchConfiguration('headless')
+    world = LaunchConfiguration('world')
+    x_pose = LaunchConfiguration('x_pose')
+    y_pose = LaunchConfiguration('y_pose')
+    z_pose = LaunchConfiguration('z_pose')
+    yaw = LaunchConfiguration('yaw')
+    config = LaunchConfiguration('config')
+    joystick = LaunchConfiguration('joystick')
+
+    # Declare the launch arguments
+    declare_x_position_cmd = DeclareLaunchArgument(
+        'x_pose', default_value='0.0',
+        description='x_pose')
+
+    declare_y_position_cmd = DeclareLaunchArgument(
+        'y_pose', default_value='0.0',
+        description='y_pose')
+
+    declare_z_position_cmd = DeclareLaunchArgument(
+        'z_pose', default_value='0.1',
+        description='z_pose')
+
+    declare_yaw_cmd = DeclareLaunchArgument(
+        'yaw', default_value='0.0',
+        description='yaw')
+
+    declare_config_cmd = DeclareLaunchArgument(
+        'config',
+        default_value=os.getenv('RIDGEBACK_CONFIG', 'base'),
+        description='get the ridgeback configuration')
+
+    declare_headless_cmd = DeclareLaunchArgument(
+        'headless',
+        default_value='False',
+        description='Whether to execute gzclient)')
+
+    declare_world_cmd = DeclareLaunchArgument(
+        'world',
+        default_value=os.path.join(this_directory, 'worlds', 'ridgeback_race.world'),
+        description='Full path to world model file to load')
+
+    declare_joy_cmd = DeclareLaunchArgument(
+        'joystick', default_value='true',
+        description='system using joystick')
+
+    # Specify the actions
+    start_gazebo_server_cmd = ExecuteProcess(
+        cmd=['gzserver', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so', world],
+        cwd=[this_directory], output='screen')
+
+    start_gazebo_client_cmd = ExecuteProcess(
+        condition=IfCondition(PythonExpression(['not ', headless])),
+        cmd=['gzclient'],
+        cwd=[this_directory], output='screen')
+
+    spawn_ridgeback_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(this_directory, 'launch', 'spawn_ridgeback.launch.py')),
+        launch_arguments={
+            'config': config,
+            'x_pose': x_pose,
+            'y_pose': y_pose,
+            'z_pose': z_pose,
+            'yaw': yaw
+        }.items()
+    )
+
+    # Create the launch description and populate
+    ld = LaunchDescription()
+
+    # Declare the launch options
+    ld.add_action(declare_headless_cmd)
+    ld.add_action(declare_world_cmd)
+    ld.add_action(declare_x_position_cmd)
+    ld.add_action(declare_y_position_cmd)
+    ld.add_action(declare_z_position_cmd)
+    ld.add_action(declare_yaw_cmd)
+    ld.add_action(declare_config_cmd)
+#  ld.add_action(declare_joy_cmd)
+
+    # Add any conditioned actions
+    ld.add_action(start_gazebo_server_cmd)
+    ld.add_action(start_gazebo_client_cmd)
+    ld.add_action(spawn_ridgeback_cmd)
+
+    return ld
---
+++ ridgeback_gazebo/launch/start_ridgeback_controls.launch.py
@@ -0,0 +1,50 @@
+# Copyright 2019 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+
+    # Declare the launch arguments
+    start_ridgeback_control = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(
+            get_package_share_directory('ridgeback_control'), 'launch', 'control.launch.py')),
+        launch_arguments={
+            'physical_robot': 'false',
+            'use_sim_time': 'true',
+        }.items()
+    )
+
+    start_ridgeback_teleop = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(
+            get_package_share_directory('ridgeback_control'), 'launch', 'teleop.launch.py')),
+        launch_arguments={
+            'physical_robot': 'false',
+            'use_sim_time': 'true',
+        }.items()
+    )
+
+    ld = LaunchDescription()
+
+    # Declare the launch options
+    ld.add_action(start_ridgeback_control)
+    ld.add_action(start_ridgeback_teleop)
+
+    return ld
